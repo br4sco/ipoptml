38b3b4924ec646a057b068c7c3c419d5a4e1c7b2
Oscar Eriksson <oerikss@kth.se>
2021-09-11 09:46:48 +0200

    Add runnable example

--- examples/runexamples.ml
+++ examples/runexamples.ml
@@ -1,6 +1,6 @@
 open Ipoptml
 
-let test_solve_problem1 () =
+let main =
   let p =
     let open Problems.Problem1 in
     create_nlp ~eval_f ~eval_grad_f ~eval_g ~jac_g_structure ~eval_jac_g
@@ -9,11 +9,4 @@ let test_solve_problem1 () =
   add_num_option p "tol" 3.82e-6 ;
   add_str_option p "mu_strategy" "adaptive" ;
   let x = Bigarray.(Array1.of_array Float64 c_layout [|1.; 5.; 5.; 1.|]) in
-  Alcotest.(check bool)
-    "solved" true
-    (solve p x |> function SolveSucceeded, _ -> true | _ -> false)
-
-let () =
-  let open Alcotest in
-  run "Ipoptml"
-    [("problems", [("solved problem 1", `Slow, test_solve_problem1)])]
+  solve p x
